5f01fb51195201f7ce3e86adb8dd8ff1696b0946
swastkk <21bce032@nith.ac.in>
2022-05-07 11:14:51 +0530

    connected__with_mailChimp_servers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+// const client = require("mailchimp-marketing");
 
 const app = express();
 
@@ -11,12 +13,46 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstName = req.body.fname;
-  var lastName = req.body.lname;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  //   console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const url = "https://us17.api.mailchimp.com/3.0/lists/5fb6f7825b";
+
+  const jsonData = JSON.stringify(data);
+
+  const options = {
+    method: "POST",
+    auth: "swastikop:487709f550fdd3d0ccb92c182805eedb-us17",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+
+  request.end();
 });
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
 });
+
+//api key 487709f550fdd3d0ccb92c182805eedb-us17
+// UNIQUE ID
+
+// uniquer id     5fb6f7825b.
